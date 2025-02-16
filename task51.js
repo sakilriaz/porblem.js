584299187e88cf1c568f74c0ec9c8bb61f7fb203
sakilriaz <sakilriaz@gmail.com>
2025-02-17 03:00:32 +0600

    task5 and task51 are added

--- task51.js
+++ task51.js
@@ -3,14 +3,18 @@
 const employees = [
     { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
     { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shikot", experience: 11, starting: 30000, increment: 1000 },
     { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
 ];
 
-let totalSalary = 0;
-for(const employee of employees){
-    const currentSalary = employee.starting + (employee.experience * employee.increment);
-    totalSalary +=currentSalary;
-    console.log(employee.name,currentSalary);
+function salary(input){
+    let totalSalary = 0;
+    for(const employee of employees){
+        // console.log(employee)
+       const currentSalary= employee.starting + (employee.experience * employee.increment);
+       totalSalary += currentSalary;
+    //    console.log(employee.name,currentSalary)
+    } return totalSalary;
 }
-console.log(totalSalary);
+const companySalary= salary(employees);
+console.log(companySalary)
